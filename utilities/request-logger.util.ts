e9a00ef27d75b6e0944f97e95c35f1b60c0d814b
Thinh Luu <quocthinhluu97@gmail.com>
2024-08-17 09:41:14 +0700

    Refactor code

--- utilities/request-logger.util.ts
+++ utilities/request-logger.util.ts
@@ -3,60 +3,82 @@ import * as allure from 'allure-js-commons';
 import { asString, generateCsv, mkConfig } from 'export-to-csv';
 import formDataToString from 'formdata-to-string'
 
+const seperateLineLength = 120;
 export default class RequestLogger {
     static async log(request: ExtendedRequest, response: ExtendedResponse): Promise<void> {
-        // console.log('Request headers', JSON.stringify(request.headers));
-        // console.log('Request payload', response.parsedBody);
-
-
         await allure.step(request.name, async () => {
-            await allure.logStep(`${request.method} ${request.pathAndQuery}`);
+            console.log();
+
+            // Log request method and query path
+            await allure.logStep(`${request.method} ${request.pathAndQuery}`)
             console.log(`${request.method} ${request.pathAndQuery}`);
 
-            if (request.headers) {
-                const requestHeaders = RequestLogger.headersToCsv(request.headers);
-                await allure.attachment('Request headers', requestHeaders, 'text/csv');
-                console.log('Request headers', JSON.stringify(request.headers));
-            }
+            // Add request to report 
+            await this.attachRequestData(request);
+            console.log();
 
-            if (request.rawBody) {
-                let attachment = JSON.stringify(request.rawBody);
-                let contentType = 'text/plain';
-
-                if (request.body instanceof FormData) {
-                    attachment = await formDataToString(request.body);
-                }
-
-                await allure.attachment('Request payload', attachment, contentType);
-                console.log('Request payload', attachment, contentType);
-            }
-
+            // Log response status
             await allure.logStep(`${response.status} ${response.statusText}`);
             console.log(`${response.status} ${response.statusText}`);
+            console.log();
 
-           const requestHeaders = RequestLogger.headersToCsv(response.headers) ;
-           await allure.attachment('Response headers', requestHeaders, 'text/csv');
-           console.info('Response', response.headers);
+            // Add response to report 
+            await this.attachResponseData(response);
 
-            if (response.bodyAsAttachment) {
-                await allure.attachment('Response body', response.bodyAsAttachment, response.contentType ?? 'text/plain');
-                console.info('Response body', response.parsedBody instanceof Blob ? '<File>' : response.bodyAsAttachment);
-            }
+            console.log();
+            console.log('-'.repeat(seperateLineLength));
         });
-
     }
+
+    private static async attachResponseData(response: ExtendedResponse) {
+        // Do not parse reponse headers to JSON because it looks urgly in allure reports
+        const requestHeaders = this.headersToCsv(response.headers);
+        await allure.attachment('Response headers', requestHeaders, 'text/csv');
+        console.log('Response', response.headers);
+        console.log();
+
+        if (response.bodyAsAttachment) {
+            await allure.attachment('Response body', response.bodyAsAttachment, response.contentType ?? 'text/plain');
+            console.log('Response body', response.parsedBody instanceof Blob ? '<File>' : response.bodyAsAttachment);
+        }
+    }
+
+    private static async attachRequestData(request: ExtendedRequest) {
+        if (request.headers) {
+            const requestHeaders = JSON.stringify(request.headers);
+            await allure.attachment('Request headers', requestHeaders, 'application/json');
+            console.log('Request headers', requestHeaders);
+            console.log();
+        }
+
+        if (request.rawBody) {
+            let requestBody = typeof request.rawBody === 'string' ? request.rawBody : JSON.stringify(request.rawBody);
+
+            let contentType = 'application/json';
+
+            if (request.body instanceof FormData) {
+                requestBody = await formDataToString(request.body);
+                contentType = 'text/plain'
+            }
+
+            await allure.attachment('Request body', requestBody, contentType);
+            console.log('Request body', requestBody, contentType);
+        }
+    }
+
 
     private static headersToCsv(headers: HeadersInit | Headers): string {
         const headerEntries = headers instanceof Headers ? Array.from(headers.entries()) : Object.entries(headers);
 
         const array = headerEntries
-            .filter(([_, value]) => typeof value === 'string') 
-            .map(([key, value]) => ({ name: key, value: value})) ?? [];
+            .filter(([_, value]) => typeof value === 'string')
+            .map(([key, value]) => ({ name: key, value: value })) ?? [];
 
         const csv = generateCsv(this.csvConfig)(array);
 
         return asString(csv);
     }
 
-    private static readonly csvConfig = mkConfig( { useKeysAsHeaders: true, showColumnHeaders: false});
+
+    private static readonly csvConfig = mkConfig({ useKeysAsHeaders: true, showColumnHeaders: false });
 }
